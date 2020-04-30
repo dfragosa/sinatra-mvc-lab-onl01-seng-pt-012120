7944935419795fd6c4d16d485df6fa4f7f8ad736
Derric Fragosa <fderric@gmail.com>
2020-04-30 17:45:39 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
+require 'pry'
 require_relative 'config/environment'
+require_relative 'models/piglatinizer'
 
 class App < Sinatra::Base
+  
+  get '/' do
+      erb :user_input
+  end
+  
+  post '/piglatinize' do 
+    user_text = params[:user_phrase]
+    @piglatin = PigLatinizer.new(params[:user_phrase])
+  end
+
 end
